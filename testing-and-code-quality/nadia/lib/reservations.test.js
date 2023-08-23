9600fbdf47c8a5defefd636051b8440aa3264459
sandenson <lgabrielsandes@gmail.com>
2023-08-23 15:47:35 -0300

    feat: Testing and Code Quality - Chapter 4 - Mocking module dependencies completed.

--- testing-and-code-quality/nadia/lib/reservations.test.js
+++ testing-and-code-quality/nadia/lib/reservations.test.js
@@ -1,7 +1,61 @@
-const reservations = require('./reservations')
 const Reservation = require('./schema/reservation')
 
+describe('fetch', () => {
+	let reservations;
+
+	beforeAll(() => {
+		jest.mock('./reservations')
+		reservations = require('./reservations')
+	})
+
+	afterAll(() => {
+		jest.unmock('./reservations')
+	})
+
+	it('should be mocked and not create a database record', () => {
+		expect(reservations.fetch()).toBeUndefined()
+	})
+})
+
+describe('save', () => {
+	let reservations;
+
+	const mockDebug = jest.fn();
+	const mockInsert = jest.fn().mockResolvedValue([1])
+
+	beforeAll(() => {
+		jest.mock('debug', () => () => mockDebug)
+		jest.mock('./knex', () => () => ({
+			insert: mockInsert,
+		}))
+
+		reservations = require('./reservations')
+	})
+
+	afterAll(() => {
+		jest.unmock('debug')
+		jest.unmock('./knex')
+	})
+
+	it('should resolve with the id upon success', async () => {
+		const value = { foo: 'bar' }
+		const expected = [1]
+
+		const actual = await reservations.save(value)
+
+		expect(actual).toStrictEqual(expected)
+		expect(mockDebug).toBeCalledTimes(1)
+		expect(mockInsert).toBeCalledWith(value)
+	})
+})
+
 describe('validate', () => {
+	let reservations;
+
+	beforeAll(() => {
+		reservations = require('./reservations')
+	})
+
 	it('should resolve with no optional fields', async () => {
 		const reservation = new Reservation({
 			date: '2017/06/10',
@@ -26,5 +80,31 @@ describe('validate', () => {
 
 		await expect(reservations.validate(reservation))
 			.rejects.toBeInstanceOf(Error)
+	})
+})
+
+describe('create', () => {
+	let reservations;
+
+	beforeAll(() => {
+		reservations = require('./reservations')
+	})
+
+	it('should reject if validation fails', async () => {
+		// Store the original
+		const original = reservations.validate;
+
+		const error = new Error('fail')
+
+		// Mock the function
+		reservations.validate = jest.fn(() => Promise.reject(error))
+
+		await expect(reservations.create())
+			.rejects.toBe(error)
+
+		expect(reservations.validate).toBeCalledTimes(1)
+
+		// Restore
+		reservations.validate = original
 	})
 })
