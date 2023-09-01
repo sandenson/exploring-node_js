8ebad1e3770df83425b3255f287434cba000d3e6
sandenson <lgabrielsandes@gmail.com>
2023-09-01 17:21:14 -0300

    feat: Node.js Essential Training 2 - Chapter 4 finished.

---
+++ essential-training_pt2/ch_04/04_01/server.js
@@ -0,0 +1,32 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 })
+
+const messages = []
+
+wss.once('listening', () => {
+    console.log('chat server waiting for connections on ws://localhost:3000');
+})
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(message.toString())
+        messages.push(message.toString())
+        if (message.toString() === 'exit') {
+            ws.close()
+        } else {
+            wss.clients.forEach(
+                client => client.send(message.toString())
+            )
+        }
+    })
+    ws.on('close', () => {
+        console.log('user disconnected')
+    })
+    console.log('new socket connected')
+    ws.send('Welcome to Live Chat!')
+    if (messages.length) {
+        ws.send('Chat currently in session')
+        messages.forEach(message => ws.send(message))
+    }
+})
---
+++ essential-training_pt2/ch_04/04_03/server.js
@@ -0,0 +1,18 @@
+import http from 'http';
+import { Server } from 'socket.io'
+
+const server = http.createServer().listen(3000)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log(`${io.engine.clientsCount} connections`)
+    socket.on('chat', (message) => {
+        console.log(`${socket.id}: ${message}`);
+        io.sockets.emit('message', message, socket.id)
+    })
+    socket.on('disconnect', () => {
+        console.log('disconnect', socket.id)
+    })
+})
+
+console.log('socket server')
